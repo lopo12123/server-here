0d3003b94fbddbb8b08edd2d5e17db2749f10359
lopo <lopo@zju.edu.cn>
2022-11-17 00:14:34 +0800

    temp: port conflict

--- src/get_user_input.rs
+++ src/get_user_input.rs
@@ -1,7 +1,9 @@
+use std::collections::{hash_map, HashMap};
 use std::env::current_dir;
 use std::io::{stdin, stdout, Write};
 use std::path::PathBuf;
 
+// 获取交互式输入
 pub struct IOController {}
 
 impl IOController {
@@ -47,5 +49,55 @@ impl IOController {
         }
 
         return root_path;
+    }
+}
+
+// 解析参数传入
+pub enum ResolverResult {
+    Port(u16),
+    Root(PathBuf),
+    Both((u16, PathBuf)),
+    Fail(String),
+}
+
+pub struct ArgsResolver {}
+
+impl ArgsResolver {
+    pub fn try_port_and_root() -> ResolverResult {
+        let mut arg_map = HashMap::new();
+        let mut args = std::env::args().collect::<Vec<String>>();
+
+        let mut p = 1;
+        while p < args.len() {
+            arg_map.insert(args[p].clone(), match args.get(p + 1) {
+                Some(n) => n.clone(),
+                None => "".to_string()
+            });
+            p += 2;
+        }
+
+        let port: u16 = match arg_map.get("-port") {
+            Some(p) => match p.parse::<u16>() {
+                Ok(p_num) => p_num,
+                Err(_) => 8000
+            },
+            None => 8000
+        };
+
+        return match arg_map.get("-root") {
+            Some(v) => {
+                let mut root = current_dir().unwrap();
+                root.push(v);
+
+                if !root.exists() {
+                    panic!("ROOT not exist!");
+                } else if !root.is_dir() {
+                    panic!("ROOT is not a directory!");
+                } else {
+                    ResolverResult::Both((port, root))
+                }
+            }
+            None => ResolverResult::Port(port),
+        };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,55 @@ mod get_user_input;
 #[macro_use]
 extern crate rocket;
 
-use std::{
-    net::{IpAddr, Ipv4Addr},
-};
+use std::{env, net::{IpAddr, Ipv4Addr}};
+use std::env::current_dir;
 use rocket::{Config, fs::FileServer};
-use get_user_input::{IOController};
+use get_user_input::{IOController, ArgsResolver, ResolverResult};
 
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
-    let port = IOController::get_port();
-    let root = IOController::get_root();
+// #[rocket::main]
+// async fn main() -> Result<(), rocket::Error> {
+//     let port = IOController::get_port();
+//     let root = IOController::get_root();
+//
+//     let configs = Config {
+//         port,
+//         address: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)).into(),
+//         ..Config::debug_default()
+//     };
+//
+//     let _rocket = rocket::custom(configs)
+//         .mount("/", FileServer::from(root))
+//         .launch().await?;
+//
+//     return Ok(());
+// }
 
-    let configs = Config {
-        port,
-        address: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)).into(),
-        ..Config::debug_default()
-    };
+fn main() {
+    let mut port = 8000;
+    let mut root = current_dir().unwrap();
 
-    let _rocket = rocket::custom(configs)
-        .mount("/", FileServer::from(root))
-        .launch().await?;
+    match ArgsResolver::try_port_and_root() {
+        ResolverResult::Port(v) => {
+            port = v;
+            root = IOController::get_root();
+            println!("1");
+        }
+        ResolverResult::Root(v) => {
+            port = IOController::get_port();
+            root = v;
+            println!("2");
+        }
+        ResolverResult::Both(v) => {
+            port = v.0;
+            root = v.1;
+            println!("3");
+        }
+        ResolverResult::Fail(v) => {
+            port = IOController::get_port();
+            root = IOController::get_root();
+            println!("4");
+        }
+    }
 
-    return Ok(());
+    println!("final: port: {}, root: {:?}", port, root);
 }
